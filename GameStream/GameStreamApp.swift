7334e87541ad8c980b24be7bcddb58dda2771d38
Javier Urbina <urbinarathgeb@gmail.com>
2023-10-15 01:08:28 -0300

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Javier Urbina on 15-10-23.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
